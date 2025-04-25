9b09c8da40d31c08d81555852054bca205243f5e
Rubén <rubensanchezmarin999@gmail.com>
2025-04-25 18:18:01 +0200

    firebase

---
+++ tfg/trabajo2/mi-app/src/hooks/useAuth.js
@@ -0,0 +1,15 @@
+// hooks/useAuth.js
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
+
+export default function useAuth() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
+  }, []);
+
+  return user;
+}
