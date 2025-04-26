8f87c26609e519fb94900e0d798fc019cfb1e067
Rubén <rubensanchezmarin999@gmail.com>
2025-04-26 21:25:22 +0200

    AjusteMerge + incorporacionCorreoApi

--- tfg/trabajo2/mi-app/src/firebase/firebaseConfig.ts
+++ tfg/trabajo2/mi-app/src/firebase/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBIkQRycpZUqXwZHme_1pP5eHp0qYifm0Y",
@@ -16,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 
 // Inicializar Authentication
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
