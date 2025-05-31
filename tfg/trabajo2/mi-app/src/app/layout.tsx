1ce690600ec5796cb5925817cde0beaa552e46bd
ManriqueCosG <manriquecos2005@gmail.com>
2025-05-31 21:20:36 +0200

    3

--- tfg/trabajo2/mi-app/src/app/layout.tsx
+++ tfg/trabajo2/mi-app/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className="h-full">
+      
       <body
         className={`
           ${geistSans.variable} ${geistMono.variable} 
